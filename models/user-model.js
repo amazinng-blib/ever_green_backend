7f41e9ff2bf870828e58052fd989005a5609e639
Nwankwo Ernest Onyebuchi <nwankwoernest2020@gmail.com>
2024-04-12 12:47:07 +0100

    fixed a bug

--- models/user-model.js
+++ models/user-model.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema({
     {
       first_name: { type: String, required: true },
       last_name: { type: String, required: true },
-      date_joined: { type: Date },
+      date_joined: { type: String },
       commission: Number,
     },
   ],
