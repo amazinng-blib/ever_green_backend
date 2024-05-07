5389a77cb1a0b229990aed75656a3debf72f26b6
Nwankwo Ernest Onyebuchi <nwankwoernest2020@gmail.com>
2024-05-07 15:23:59 +0100

    added some api's

--- routes/transaction-routes.js
+++ routes/transaction-routes.js
@@ -10,7 +10,7 @@ const router = require('express').Router();
 
 router.post('/withdrawal-request', verifyToken, requestForWithdrawal);
 router.put('/respond-to-withdrawal', verifyToken, respondToWithdrawalRequest);
-router.put('submit-wallet-address', verifyToken, submitWalletAddress);
+router.put('/submit-wallet-address', verifyToken, submitWalletAddress);
 router.get('/withdrawal-request', verifyToken, getAllWithdrawalRequest);
 
 module.exports = router;
